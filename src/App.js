3cf85595917d50c27da01d6d57b542ea8c20922a
gitBanerjee <sourav.itsme777@gmail.com>
2022-01-17 00:57:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import _ from "lodash";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import "./App.css";
+import { Oval } from "react-loader-spinner";
+import { loadMovie, addMovie, removeMovie } from "./reducers";
+import store from "./store";
 
 function App() {
+  const movies = useSelector((state) => _.get(state, "movies"));
+  const loading = useSelector((state) => _.get(state, "loading"));
+  const [name, setName] = useState();
+  const [comment, setComment] = useState();
+  const [genre, setGenre] = useState("Action");
+  const [rating, setRating] = useState("0");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadMovie());
+  }, []);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (name && comment) {
+      const id = (movies.length + _.random(5000)).toString();
+      const movie = { id, name, comment, genre, rating };
+      console.log(movie);
+      dispatch(addMovie(movie));
+    }
+    console.log(store.getState());
+  };
+
+  const onEditHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const onDeleteHandler = (id) => {
+    const movieToDelete = _.find(movies, (el) => el.id === id);
+    dispatch(removeMovie(movieToDelete));
+    console.log(id);
+  };
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleComment = (e) => {
+    setComment(e.target.value);
+  };
+  const onChangeGenre = (e) => {
+    setGenre(e.target.value);
+  };
+  const onChangeRating = (e) => {
+    setRating(e.target.value);
+  };
+  const renderSection = _.map(movies, ({ id, name, comment, rating }) => {
+    return (
+      <div key={id} className="container">
+        <div className="name">
+          <em className="em">{name}</em>
+          {[...Array(Number(rating))].map((e, i) => (
+            <span key={i} className="fa fa-star checked"></span>
+          ))}
+          {[...Array(5 - Number(rating))].map((e, i) => (
+            <span key={i} className="fa fa-star"></span>
+          ))}
+          <div className="mini-container">
+            <button
+              class="button button-outline"
+              style={{ color: "purple" }}
+              onClick={onEditHandler}
+            >
+              Edit
+            </button>
+            <button
+              class="button button-outline"
+              style={{ color: "purple" }}
+              onClick={() => onDeleteHandler(id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+        <blockquote className="details">
+          <em>{comment}</em>
+        </blockquote>
+      </div>
+    );
+  });
+
+  if (loading)
+    return (
+      <div className="App">
+        <Oval color="#00BFFF" height={500} width={500} />
+      </div>
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <form>
+          <fieldset>
+            <label for="nameField">Movie Name</label>
+            <input
+              type="text"
+              placeholder="Enter Movie Name Here"
+              id="nameField"
+              onChange={(e) => handleName(e)}
+            />
+            <label for="commentField">Movie Details</label>
+            <textarea
+              placeholder="Enter Movie Details Here"
+              id="commentField"
+              onChange={(e) => handleComment(e)}
+            ></textarea>
+            <label for="genre">Genre</label>
+            <select id="genre" onChange={(e) => onChangeGenre(e)}>
+              <option value="Action">Action</option>
+              <option value="Comedy">Comedy</option>
+              <option value="Thriller">Thriller</option>
+              <option value="Horror">Horror</option>
+            </select>
+            <label for="rating">Rating</label>
+            <select id="rating" onChange={(e) => onChangeRating(e)}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <input
+              class="button-primary"
+              type="submit"
+              onClick={onSubmitHandler}
+            />
+          </fieldset>
+        </form>
+      </div>
+      {renderSection}
+    </React.Fragment>
   );
 }
 
---
+++ src/reducers/index.js
@@ -0,0 +1,76 @@
+import _ from "lodash";
+import axios from "axios";
+
+//initial state
+const intialState = {
+  movies: [],
+  loading: false,
+  error: undefined,
+};
+
+//actions
+export const ADD_MOVIE = "ADD_MOVIE";
+export const REMOVE_MOVIE = "REMOVE_MOVIE";
+export const UPDATE_MOVIE = "UPDATE_MOVIE";
+export const FETCH_MOVIE = "FETCH_MOVIE";
+export const LOAD_MOVIE = "LOAD_MOVIE";
+
+//action creators
+
+export const loadMovie = () => {
+  return async (dispatch) => {
+    dispatch({ type: LOAD_MOVIE, payload: [] });
+    setTimeout(async () => {
+      const { data } = await axios.get("http://localhost:3006/movies");
+      dispatch({ type: FETCH_MOVIE, payload: data });
+    }, 5000);
+  };
+};
+
+export const addMovie = (movie) => {
+  return async (dispatch) => {
+    dispatch({ type: LOAD_MOVIE, payload: [] });
+    await axios.post("http://localhost:3006/movies", movie);
+    dispatch({ type: ADD_MOVIE, payload: movie });
+  };
+};
+
+export const removeMovie = (movie) => {
+  return async (dispatch) => {
+    dispatch({ type: LOAD_MOVIE, payload: [] });
+    await axios.delete(`http://localhost:3006/movies/${movie.id}`);
+    dispatch({ type: REMOVE_MOVIE, payload: movie });
+  };
+};
+
+export const updateMovie = (movie) => {
+  return async (dispatch) => {
+    await axios.post("http://localhost:3006/movies", movie);
+    dispatch({ type: ADD_MOVIE, payload: movie });
+  };
+};
+
+export default function rootReducer(state = intialState, action) {
+  switch (action.type) {
+    case LOAD_MOVIE:
+      return { ...state, loading: true };
+    case FETCH_MOVIE:
+      return { ...state, movies: action.payload, loading: false };
+    case ADD_MOVIE:
+      return {
+        ...state,
+        movies: [...state.movies, action.payload],
+        loading: false,
+      };
+    case REMOVE_MOVIE:
+      const updatedMovies = _.filter(
+        state.movies,
+        (el) => el.id !== action.payload.id
+      );
+      return { ...state, movies: updatedMovies, loading: false };
+    case UPDATE_MOVIE:
+      return { ...state, movies: [...state.movies, action.payload] };
+    default:
+      return state;
+  }
+}
